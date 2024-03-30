d777438ee2be22b60b08cf84822bad402a1a5cca
PaaulCB <paul.bordeanu.3d@gmail.com>
2024-03-30 20:05:52 +0000

    Add secret_key

--- heavenpizza/settings.py
+++ heavenpizza/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-01yujg2n3m8i6agq=$&uo^=n)n4e7e6i39c=7p7do(rdnj=3k8'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
