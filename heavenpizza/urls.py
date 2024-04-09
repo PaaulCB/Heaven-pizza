c04f7ace25437750123d690554e8115fbd2bf641
PaaulCB <paul.bordeanu.3d@gmail.com>
2024-04-09 20:57:01 +0000

    Add cloudinary and summernote

--- heavenpizza/urls.py
+++ heavenpizza/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core.views import home
 from menu.views import view_menu
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('menu/', view_menu, name='menu'),
+    path('summernote/', include('django_summernote.urls')),
     path('', home, name='home'),
 ]
