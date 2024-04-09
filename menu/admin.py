d292bf280448970671360688187bb5cb84914220
PaaulCB <paul.bordeanu.3d@gmail.com>
2024-04-09 22:22:41 +0000

    Add MenuItem model

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import MenuItem
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+@admin.register(MenuItem)
+class MenuItemAdmin(SummernoteModelAdmin):
+    list_display = ('name', 'price', 'kcal', 'type', 'badge')
+    search_fields = ['name','type','price']
+    list_filter = ('type','badge')
+    summernote_fields = ('description')
--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
+# Type choices
+STARTER = 'Starter'
+ENTREE = 'Entree'
+DESSERT = 'Dessert'
+DRINK = 'Drink'
+TYPE = ((STARTER, "Starter"), (ENTREE, "Entree"), (DESSERT, "Dessert"), (DRINK, "Drink"))
 # Create your models here.
+class MenuItem(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    kcal = models.IntegerField()
+    type = models.CharField(max_length=15, choices=TYPE, default=STARTER)
+    photo = CloudinaryField('image', default='placeholder')
+    badge = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return f"{self.name} | {self.type}"
