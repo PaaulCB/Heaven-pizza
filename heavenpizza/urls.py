6e476d531dc771276f9d13b8f0662618c8083cec
PaaulCB <paul.bordeanu.3d@gmail.com>
2024-04-15 11:09:08 +0000

    Add authentication

--- heavenpizza/urls.py
+++ heavenpizza/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from core.views import home
 
 urlpatterns = [
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path("location/", include("location.urls"), name="location-urls"),
     path("menu/", include("menu.urls"), name="menu-urls"),
